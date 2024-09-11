b29934fac2b0c3fba6d9815a0990a6e4b3466b2a
Maria Carolina <demycar24@gmail.com>
2024-09-11 11:00:18 -0300

    :lipstick: feat: Organizando as pastas/Inserindopasta introducao ao JS

---
+++ JavaScript/introducaoJs/script.js
@@ -0,0 +1,36 @@
+console.log("Bom dia T1");
+
+// comentario de apenas 1 linha
+/*
+comentário de varias linhas
+*/
+//  variaveis lugar onde iremos guardar informações
+// Declarando variaveis: var, let e const
+
+// variavel + nome da variavel + = + valor;
+//  criando nomes de variaveis:
+//  não podemos usar nomes com espaços
+//podemos usar camelCase - escrever duas ou mais palavras juntas, primeira palavra vai iniciar com a letra minuscula, seguida das outras palavras com a com primeira letra maiuscula
+//case sensitive # letras minusculas de letras maiusculas
+
+// variavel + nome da variavel + = + valor;
+// preferida e podemos reatribuir um valor
+// string "texto"
+//
+let nomeDoAluno = "Luan Vieira";
+nomeDoAluno = "Alexandra";
+nomeDoAluno = "Gislane";
+nomeDoAluno = "Stefany";
+console.log(nomeDoAluno);
+
+// var vc consegue reescrever e tbm reatribuir
+var facilitador = "Kleber";
+var facilitador = "Débora";
+facilitador = "Fellipe";
+console.log(facilitador);
+
+// constante não pode ser alterada:
+const comida = "Batata";
+//const instrutora =" Carol"
+//instrutora = "José"
+
