505eea750a20db2757798209b038bfd7a795cccf
Maria Carolina <demycar24@gmail.com>
2024-09-16 09:02:18 -0300

    :sparkles: feat: Adionando a pasta do Loop

---
+++ JavaScript/loop/script.js
@@ -0,0 +1,48 @@
+//repetição
+
+// console.log(1);
+// console.log(2);
+
+// ++  atribui um incremento (valor da variavel + 1)
+// -- atribui um decremento (valor da variavel - 1)
+
+//operadores de atribuição;
+//  = atribuição
+let saldo = 100;
+saldo = 10;
+console.log(saldo);
+
+
+// += atribuição de adição
+//  atribui e soma um a valor a uma variavel
+console.log((saldo += 10));
+// -= atribuição de subtração
+// atribui e diminui um valor
+
+
+// Loops são laços de repetição
+// estrutura do for
+// for() dentro dos parenteses vamos passar 3 expressões:
+
+// for(inicializador;) Uma variavel que vai armazenar uma
+// valor inicial / let i = 0;
+
+//  SEGUNDA EXPRESSÃO - condição: i <= 5;
+
+// TERCEIRA EXPRESSÃO - itereção incremento/ decremento
+
+// for (estrutura){
+//     executado
+// }
+
+// for(let i = 0; i <=10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 2; i <= 12; i += 2) {
+//   console.log(i); // retorna numeros pares até 12
+// }
+
+// for (let i = 10; i <= 100; i += 10) {
+//   console.log(i); // retorna numeros de 10 em 10
+// }
